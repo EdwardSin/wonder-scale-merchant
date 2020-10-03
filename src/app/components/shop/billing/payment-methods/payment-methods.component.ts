c8b98bdf52374264d4bfd45cfdcd46019145c9d8
Edward Sin <edwardsin99@hotmail.com>
2020-10-04 00:36:26 +0800

    .: Payment Methods (WSM-189)

--- src/app/components/shop/billing/payment-methods/payment-methods.component.ts
+++ src/app/components/shop/billing/payment-methods/payment-methods.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { PaymentCard } from '@objects/payment-card';
-import { AuthCardContributorService } from '@services/http/auth-shop/contributor/auth-card-contributor.service';
-import { SharedCardService } from '@services/shared/shared-card.service';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AuthPaymentMethodAdminService } from '@services/http/auth-shop/admin/auth-payment-method-admin.service';
 import { SharedShopService } from '@services/shared/shared-shop.service';
 import { DocumentHelper } from '@helpers/documenthelper/document.helper';
 import { WsLoading } from '@elements/ws-loading/ws-loading';
 import { WsToastService } from '@elements/ws-toast/ws-toast.service';
 import _ from 'lodash';
+import * as braintree from 'braintree-web';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'payment-methods',
@@ -16,52 +16,172 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./payment-methods.component.scss']
 })
 export class PaymentMethodsComponent implements OnInit {
-  cards: PaymentCard[] = [];
+  paymentMethods = [];
   loading: WsLoading = new WsLoading;
   currency: string = '';
+  modifyCardModalOpened: boolean;
+  paymentInstance;
+  modifyPaymentMethodModalOpened: boolean;
+  paymentGatewayLoading: WsLoading = new WsLoading;
+  defaultLoading: WsLoading = new WsLoading;
+  changeDefaultPaymentMethodIndex: number = -1;
+  removePaymentMethodIndex: number = -1;
+  cardholderName: string = '';
+  makeDefault: boolean = true;
   private ngUnsubscribe: Subject<any> = new Subject;
   constructor(
-    private authCardContributorService: AuthCardContributorService,
-    private sharedCardService: SharedCardService,
-    private sharedShopService: SharedShopService) { }
+    private authPaymentMethodAdminService: AuthPaymentMethodAdminService,
+    private sharedShopService: SharedShopService,
+    private ref: ChangeDetectorRef) { }
 
   ngOnInit() {
     let shop_name = this.sharedShopService.shop_name;
     DocumentHelper.setWindowTitleWithWonderScale('Payment Methods - ' + shop_name);
-    this.getCardsByShopId();
-    this.sharedCardService.cards.pipe(takeUntil(this.ngUnsubscribe))
-      .subscribe(result => {
-        if (result) {
-          this.cards = result;
+    this.getPaymentMethodsByShopId();
+  }
+  ngAfterViewInit() {
+  }
+  createPaymentForm() {
+    var authorization = environment.braintreeAuthorization;
+    var me = this;
+    this.paymentGatewayLoading.start();
+    me.ref.detectChanges();
+    braintree.client.create({
+      authorization: authorization
+    }, function (err, clientInstance) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      createHostedFields(clientInstance);
+    });
+
+    function createHostedFields(clientInstance) {
+      braintree.hostedFields.create({
+        client: clientInstance,
+        styles: {
+          'input': {
+            'font-size': '14px',
+            'font-weight': 'lighter',
+            'color': '#000'
+          }
+        },
+        fields: {
+          number: {
+            selector: '#card-number',
+            placeholder: '---- ---- ---- ----'
+          },
+          cvv: {
+            selector: '#cvv',
+            placeholder: '123'
+          },
+          expirationDate: {
+            selector: '#expiration-date',
+            placeholder: 'MM/YYYY'
+          },
+          postalCode: {
+            selector: '#postal-code',
+            placeholder: 'Postal Code'
+          }
         }
-      })
-  }
-  getCardsByShopId() {
-    this.loading.start();
-    this.authCardContributorService.getCardsByShopId().pipe(takeUntil(this.ngUnsubscribe))
-      .subscribe(result => {
-        this.sharedCardService.cards.next(result.result);
-        this.loading.stop();
-      }, err => {
-        this.loading.stop();
-      })
-  }
-  deleteCard(id) {
-    if (confirm('Are you sure to delete the card?')) {
-      this.authCardContributorService.deleteCard(id).pipe(takeUntil(this.ngUnsubscribe))
-        .subscribe(result => {
-          _.remove(this.cards, card => card._id == id);
-          WsToastService.toastSubject.next({ content: 'Card is deleted successfully!', type: 'success' });
-        })
+      }, function (err, instance) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        me.paymentInstance = instance;
+        me.paymentGatewayLoading.stop();
+
+      });
     }
   }
-  setDefault(id) {
-    if (confirm('Are you sure to set the card as default?')) {
-      this.authCardContributorService.setDefault(id).pipe(takeUntil(this.ngUnsubscribe))
+  getPaymentMethodsByShopId() {
+    this.loading.start();
+    //read cards from payment gateway get cards information.
+    this.authPaymentMethodAdminService.getPaymentMethodsByShopId().pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(result => {
+        this.paymentMethods = result['result'];
+        this.loading.stop();
+      });
+  }
+  openModifyPaymentMethodModal() {
+    this.modifyPaymentMethodModalOpened = true;
+    this.ref.detectChanges();
+    this.createPaymentForm();
+  }
+  addPaymentMethod() {
+    this.loading.start();
+    let instance = this.paymentInstance;
+    if (!this.cardholderName || this.cardholderName.trim() == '') {
+      WsToastService.toastSubject.next({ content: 'Please enter cardholder name!' });
+      return;
+    }
+    instance.tokenize((tokenizeErr, payload) => {
+      if (tokenizeErr) {
+        this.loading.stop();
+        if (tokenizeErr.details == undefined) {
+          WsToastService.toastSubject.next({ content: 'Postal code is required!', type: 'danger' })
+        } else if (tokenizeErr.details && tokenizeErr.details.invalidFieldKeys && tokenizeErr.details.invalidFieldKeys.length) {
+          if (tokenizeErr.details.invalidFieldKeys[0] == 'postalCode') {
+            WsToastService.toastSubject.next({ content: 'Postal code is invalid!', type: 'danger' })
+          } else if (tokenizeErr.details.invalidFieldKeys[0] == 'number') {
+            WsToastService.toastSubject.next({ content: 'Card number is invalid!', type: 'danger' })
+          } else if (tokenizeErr.details.invalidFieldKeys[0] == 'cvv') {
+            WsToastService.toastSubject.next({ content: 'CVV is invlid!', type: 'danger' })
+          } else if (tokenizeErr.details.invalidFieldKeys[0] == 'expirationDate') {
+            WsToastService.toastSubject.next({ content: 'Card expiration date is invalid!', type: 'danger' })
+          }
+        }
+        console.error(tokenizeErr);
+        return;
+      } else {
+        this.authPaymentMethodAdminService.addPaymentMethod({ paymentMethodNonce: payload.nonce, cardholderName: this.cardholderName, makeDefault: this.makeDefault }).pipe(takeUntil(this.ngUnsubscribe))
+          .subscribe(result => {
+            this.loading.stop();
+            this.getPaymentMethodsByShopId();
+            this.modifyPaymentMethodModalOpened = false;
+            this.cardholderName = '';
+          });
+      }
+    });
+  }
+  confirmAddPaymentMethod() {
+    this.addPaymentMethod();
+  }
+  deletePaymentMethod(paymentMethod, index) {
+    let { token } = paymentMethod;
+    if (paymentMethod.default) {
+      WsToastService.toastSubject.next({ content: 'Default payment method cannot be deleted!', type: 'danger' });
+      return;
+    }
+    if (confirm('Are you sure to delete the payment method?')) {
+      this.removePaymentMethodIndex = index;
+      this.authPaymentMethodAdminService.removePaymentMethod({ token }).pipe(takeUntil(this.ngUnsubscribe))
         .subscribe(result => {
-          this.getCardsByShopId();
-          WsToastService.toastSubject.next({ content: 'Card is set as default!', type: 'success' });
-        })
+          this.paymentMethods = this.paymentMethods.filter(paymentMethod => {
+            return paymentMethod.token != token;
+          })
+          WsToastService.toastSubject.next({ content: 'Payment method is deleted successfully!', type: 'success' });
+          this.removePaymentMethodIndex = -1;
+          this.loading.stop();
+        });
+    }
+  }
+  setDefaultPaymentMethod(token, index) {
+    if (confirm('Are you sure to set the payment method as default?')) {
+      this.defaultLoading.start();
+      this.changeDefaultPaymentMethodIndex = index;
+      this.authPaymentMethodAdminService.setDefaultPaymentMethod({ token }).pipe(takeUntil(this.ngUnsubscribe))
+        .subscribe(result => {
+          this.getPaymentMethodsByShopId();
+          this.changeDefaultPaymentMethodIndex = -1;
+          this.defaultLoading.stop();
+          this.loading.stop();
+        }, err => {
+          this.changeDefaultPaymentMethodIndex = -1;
+          this.defaultLoading.stop();
+          this.loading.stop();
+        });
     }
   }
 }
--- src/app/components/shop/billing/subscription/subscription.component.ts
+++ src/app/components/shop/billing/subscription/subscription.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { SharedShopService } from '@services/shared/shared-shop.service';
 import { DocumentHelper } from '@helpers/documenthelper/document.helper';
 import { Subject } from 'rxjs';
+import { AuthPackageAdminService } from '@services/http/auth-shop/admin/auth-package-admin.service';
+import { Package } from '@objects/package';
+import { WsLoading } from '@elements/ws-loading/ws-loading';
+import { takeUntil } from 'rxjs/operators';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'subscription',
@@ -9,13 +14,87 @@ import { Subject } from 'rxjs';
   styleUrls: ['./subscription.component.scss']
 })
 export class SubscriptionComponent implements OnInit {
-
+  subscription;
+  username;
+  package: Package;
+  loading: WsLoading = new WsLoading;
+  changePackageLoading: WsLoading = new WsLoading;
+  selectedProduct;
+  unsubscribeLoading: WsLoading = new WsLoading;
+  isSubscriptionOpened: boolean;
+  isUnsubscribedProductConfirmationOpenend: boolean;
   private ngUnsubscribe: Subject<any> = new Subject;
-  constructor(private sharedShopService: SharedShopService) { }
-
-  ngOnInit() {
-    let shop_name = this.sharedShopService.shop_name;
-    DocumentHelper.setWindowTitleWithWonderScale('Subscription - ' + shop_name);
+  constructor(private sharedShopService: SharedShopService,
+    private authPackageAdminService: AuthPackageAdminService) { 
+  } 
+    ngOnInit() {
+      let shop_name = this.sharedShopService.shop_name;
+      this.username = this.sharedShopService.shop_username;
+      DocumentHelper.setWindowTitleWithWonderScale('Subscription - ' + shop_name);
+      this.getShopPackage();
   }
-
+  getShopPackage() {
+    this.loading.start();
+    this.authPackageAdminService.getShopPackages().pipe(takeUntil(this.ngUnsubscribe)).
+    subscribe(result => {
+      this.package = result['result'];
+      if (this.package.next) {
+        this.subscription = this.package.next;
+      }
+      this.loading.stop();
+    })
+  }
+  changePackage() {
+    let obj = {
+      nextPackage: this.subscription
+    };
+    this.changePackageLoading.start();
+    // this.authPackageAdminService.changePackage(obj).pipe(takeUntil(this.ngUnsubscribe), finalize(() => {this.changePackageLoading.stop()})).subscribe(result => {
+    //   if (result['result']) {
+    //     this.getShopPackage();
+    //     this.package.next = this.subscription;
+    //     this.isSubscriptionOpened = false;
+    //   }
+    // });
+  }
+  unsubscribe() {
+    let obj = {
+      _id: this.selectedProduct._id
+    };
+    this.unsubscribeLoading.start();
+    // this.authPackageAdminService.unsubscribeProduct(obj).pipe(takeUntil(this.ngUnsubscribe), finalize(() => {this.unsubscribeLoading.stop()})).subscribe(result => {
+    //   if (result['result']) {
+    //     this.getShopPackage();
+    //     this.selectedProduct.subscribe = false;
+    //     this.isUnsubscribedProductConfirmationOpenend = false;
+    //   }
+    // });
+  }
+  isExpired(product) {
+    return product.expiryDate <= new Date();
+  }
+  openUnsubscribe(product) {
+    this.isUnsubscribedProductConfirmationOpenend = true;
+    this.selectedProduct = product;
+  }
+  get nextTotal() { 
+    let total = 0;
+    if (!this.package.forfeit) {
+      switch (this.package.next) {
+        case 'monthly':
+          total += 18.80; break;
+        case '6-month':
+          total += 100.80; break;
+        case 'yearly':
+          total += 177.60; break;
+      }
+    }
+    total += _.sumBy(this.package.products, function (product) {
+      if (product && product.subscribe) {
+        return product.price;
+      }
+      return 0;
+    });
+    return total;
+  }
 }
