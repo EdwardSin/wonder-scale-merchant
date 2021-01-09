59c92ee8fa1af1c088138ab4c945df353aefd2b9
Edward Sin <edwardsin99@hotmail.com>
2021-01-09 16:23:20 +0800

    .: Set Title - invoice (WSM-288)

--- src/app/components/store/invoices/accounting/accounting.component.ts
+++ src/app/components/store/invoices/accounting/accounting.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DocumentHelper } from '@helpers/documenthelper/document.helper';
+import { SharedStoreService } from '@services/shared/shared-store.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-accounting',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountingComponent implements OnInit {
 
-  constructor() { }
+  private ngUnsubscribe: Subject<any> = new Subject;
+  constructor(
+    private sharedStoreService: SharedStoreService
+  ) { }
 
   ngOnInit(): void {
+    this.sharedStoreService.store.pipe(takeUntil(this.ngUnsubscribe)).subscribe(result => {
+      if (result) {
+        DocumentHelper.setWindowTitleWithWonderScale('Accounting - ' + result.name);
+      }
+    });
   }
 
 }
