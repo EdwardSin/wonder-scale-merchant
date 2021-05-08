15f4df5b6da8516ad913d76ae50fe918e02a380c
Edward Sin <edwardsin99@hotmail.com>
2021-05-08 20:08:33 +0800

    .: Update invoice-setup to show check (WSM-232)

--- src/app/modules/store/invoices/invoice-setup/invoice-setup.module.ts
+++ src/app/modules/store/invoices/invoice-setup/invoice-setup.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { InvoiceSetupRoutingModule } from './invoice-setup-routing.module';
 import { InvoiceSetupComponent } from '@components/store/invoices/invoice-setup/invoice-setup.component';
+import { ElementModule } from 'src/app/modules/public/element/element.module';
 
 
 @NgModule({
   declarations: [InvoiceSetupComponent],
   imports: [
     CommonModule,
+    ElementModule,
     InvoiceSetupRoutingModule
   ]
 })
