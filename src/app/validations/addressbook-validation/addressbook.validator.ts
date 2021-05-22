25bfbd6bbdb4fe76da85ad489b6daf39e9dfae0b
Edward Sin <edwardsin99@hotmail.com>
2021-05-22 17:38:34 +0800

    .: Update phone validation (WSW-115)

--- src/app/validations/addressbook-validation/addressbook.validator.ts
+++ src/app/validations/addressbook-validation/addressbook.validator.ts
@@ -2,7 +2,7 @@ import { WsToastService } from '@elements/ws-toast/ws-toast.service';
 
 export class AddressBookValidator {
     form;
-    regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+    // regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
     constructor(form) {
         this.form = form;
     }
@@ -16,10 +16,10 @@ export class AddressBookValidator {
             WsToastService.toastSubject.next({ content: 'Please enter the phone number!', type: 'danger'});
             return false;
         }
-        else if (!this.regex.test(form.value.phone.trim())) {
-            WsToastService.toastSubject.next({ content: 'Please enter a valid phone number!', type: 'danger'});
-            return false;
-        }
+        // else if (!this.regex.test(form.value.phone.trim())) {
+        //     WsToastService.toastSubject.next({ content: 'Please enter a valid phone number!', type: 'danger'});
+        //     return false;
+        // }
         else if (!form.value.address.trim()) {
             WsToastService.toastSubject.next({ content: 'Please enter your address!', type: 'danger'});
             return false;
