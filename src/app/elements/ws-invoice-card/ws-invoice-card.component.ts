e59033951f3a3b2125574ed7ae4a4ace672c919e
Edward Sin <edwardsin99@hotmail.com>
2021-07-20 16:50:29 +0800

    .: Add message note before send to merchant (WSM-410)

--- src/app/elements/ws-invoice-card/ws-invoice-card.component.ts
+++ src/app/elements/ws-invoice-card/ws-invoice-card.component.ts
@@ -18,6 +18,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class WsInvoiceCardComponent implements OnInit {
   @Input() item;
   @Input() onInvoiceInfoModalClick: Function;
+  @Input() onInvoiceMessageClick: Function;
   environment = environment;
   hours = ['06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '00', '01', '02', '03', '04', '05'];
   mins = ['00', '15', '30', '45'];
